462fc695879e2db279377ca27135742608b8e4c3
Alexander Kramer <a.kramer@reply.de>
2023-01-31 07:59:52 +0100

    :poop: add imperative solution for Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -7,6 +7,12 @@ fun main() {
         return top1.sum()
     }
 
+    fun part1Alternative(input: List<String>): Int {
+        val summedBags = input.toSummedBags()
+        val top1 = summedBags.topNBags(1)
+        return top1.sum()
+    }
+
     fun part2(input: String): Int {
         val bags = input.toBags()
         val summedBags = bags.deepSum()
@@ -14,16 +20,34 @@ fun main() {
         return top3.sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInputPart1 = readInputAsText("Day01_test")
-    check(part1(testInputPart1) == 24_000)
+    fun part2Alternative(input: List<String>): Int {
+        val summedBags = input.toSummedBags()
+        val top3 = summedBags.topNBags(3)
+        return top3.sum()
+    }
 
-    val testInputPart2 = readInputAsText("Day01_test")
-    check(part2(testInputPart2) == 45_000)
+    // test input
+    val testInputPart = readInputAsText("Day01_test")
+    val testInputPartAlternative = readInputAsLines("Day01_test")
 
+    // test part 1: if implementation meets criteria from the description, like:
+    check(part1(testInputPart) == 24_000)
+    // alternative
+    check(part1Alternative(testInputPartAlternative) == 24_000)
+
+
+    // test part 2: if implementation meets criteria from the description, like:
+    check(part2(testInputPart) == 45_000)
+    // alternative
+    check(part2Alternative(testInputPartAlternative) == 45_000)
+
+    // use the given input and print the result
     val input = readInputAsText("Day01")
+    val inputAlternative = readInputAsLines("Day01")
     part1(input).println()
+    part1Alternative(inputAlternative).println()
     part2(input).println()
+    part2Alternative(inputAlternative).println()
 }
 
 internal fun String.toBags() = split("\n\n")
@@ -33,3 +57,20 @@ internal fun List<List<Int>>.deepSum() = map { bag -> bag.sum() }
 
 fun List<Int>.topNBags(n: Int) = sortedDescending()
         .take(n)
+
+internal fun List<String>.toSummedBags(): List<Int> {
+    val result = mutableListOf<Int>()
+    var summedBag = 0
+
+    for (line in this) {
+        val lineAsInt = line.toIntOrNull()
+        if(lineAsInt != null) {
+            summedBag += lineAsInt
+        } else {
+            result.add(summedBag)
+            summedBag = 0
+        }
+    }
+    result.add(summedBag)
+    return result
+}
