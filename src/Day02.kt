88c40430a0ab6fb47cfb965a4507bb0eda6345b9
Alexander Kramer <a.kramer@reply.de>
2023-01-31 19:34:18 +0100

    :sparkles: Solution for Day 02 part 1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,14 +1,27 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-        // The first column is what your opponent is going to play: A for Rock, B for Paper, and C for Scissors.
-        // The second column, you reason, must be what you should play in response: X for Rock, Y for Paper, and Z for Scissors.
-        // Your total score is the sum of your scores for each round.
-        // The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors)
-        // plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won)
-       return input.size
-    }
+        val scores = input.map { round ->
+            round.split(" ").let { (pickOpponent, pickPlayer) ->
+                val opponent = pickOpponent.toRockPaperScissors()
+                val player = pickPlayer.toRockPaperScissors()
+                val scoreForSelection = Score(
+                    opponent = opponent.toSelectionScore(),
+                    player = player.toSelectionScore()
+                )
+                val scoreForOutcomeOfTheRound = Score(
+                    opponent = opponent.vs(player),
+                    player = player.vs(opponent)
+                )
+                scoreForSelection + scoreForOutcomeOfTheRound
+            }
+        }.fold(Score(
+            opponent = 0,
+            player = 0
+        )) { acc, cur -> acc + cur }
 
+        return scores.player
+    }
 
     fun part2(input: List<String>): Int {
         return input.size
@@ -21,10 +34,52 @@ fun main() {
     check(part1(testInput) == 15)
 
     // test part 2: if implementation meets criteria from the description, like:
-    check(part2(testInput) == 45_000)
+    // check(part2(testInput) == 45_000)
 
     // use the given input and print the result
     val input = readInputAsLines("Day02")
     part1(input).println()
     part2(input).println()
 }
+
+fun String.toRockPaperScissors(): RockPaperScissors = when (this) {
+    "A", "X" -> RockPaperScissors.ROCK
+    "B", "Y" -> RockPaperScissors.PAPER
+    "C", "Z" -> RockPaperScissors.SCISSORS
+    else -> error("String $this can't be converted to RockPaperScissors")
+}
+
+enum class RockPaperScissors {
+    ROCK, PAPER, SCISSORS
+}
+
+private fun RockPaperScissors.toSelectionScore(): Int = when (this) {
+    RockPaperScissors.ROCK -> 1
+    RockPaperScissors.PAPER -> 2
+    RockPaperScissors.SCISSORS -> 3
+}
+
+private fun RockPaperScissors.vs(other: RockPaperScissors): Int {
+    return when {
+        this == RockPaperScissors.ROCK && other == RockPaperScissors.ROCK -> 3
+        this == RockPaperScissors.ROCK && other == RockPaperScissors.PAPER -> 0
+        this == RockPaperScissors.ROCK && other == RockPaperScissors.SCISSORS -> 6
+        this == RockPaperScissors.PAPER && other == RockPaperScissors.ROCK -> 6
+        this == RockPaperScissors.PAPER && other == RockPaperScissors.PAPER -> 3
+        this == RockPaperScissors.PAPER && other == RockPaperScissors.SCISSORS -> 0
+        this == RockPaperScissors.SCISSORS && other == RockPaperScissors.ROCK -> 0
+        this == RockPaperScissors.SCISSORS && other == RockPaperScissors.PAPER -> 6
+        this == RockPaperScissors.SCISSORS && other == RockPaperScissors.SCISSORS -> 3
+        else -> error("Invalid combination $this and $other")
+    }
+}
+
+data class Score(val opponent: Int, val player: Int) {
+    operator fun plus(other: Score): Score {
+        return Score(
+            opponent = this.opponent + other.opponent,
+            player = this.player + other.player
+        )
+    }
+}
+
