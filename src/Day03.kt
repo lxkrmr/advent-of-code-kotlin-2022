d03b72aa2b79c43831ea58d6589c81fa912d6fa2
Alexander Kramer <a.kramer@reply.de>
2023-02-01 19:38:56 +0100

    :sparkles: solved Day 03 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,16 +1,20 @@
 fun main() {
 
-    fun part1(input: List<String>): Int {
-        return input.flatMap { rucksack ->
+    fun part1(input: List<String>): Int =
+        input.flatMap { rucksack ->
             val (firstCompartment, secondCompartment) = rucksack.chunked(rucksack.length / 2)
-            val commonChars = firstCompartment.filter { char -> char in secondCompartment }.toSet()
-            commonChars.map { char -> char.toPriority() }
+            val common = firstCompartment.filter { item -> item in secondCompartment }.toSet()
+            common.map { item -> item.toPriority() }
         }.sum()
-    }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+        input.chunked(3)
+                .flatMap { groupOfThree ->
+                    val (first, second, third) = groupOfThree
+                    val commonBetweenFirstAndSecond = first.filter { item -> item in second }.toSet()
+                    val commonBetweenAll = commonBetweenFirstAndSecond.filter { item -> item in third }
+                    commonBetweenAll.map { item -> item.toPriority() }
+                }.sum()
 
     // test input
     val testInput = readInputAsLines("Day03_test")
@@ -19,12 +23,12 @@ fun main() {
     check(part1(testInput) == 157)
 
     // test part 2: if implementation meets criteria from the description, like:
-    // check(part2(testInput) == 12)
+    check(part2(testInput) == 70)
 
     // use the given input and print the result
     val input = readInputAsLines("Day03")
     part1(input).println()
-    // part2(input).println()
+    part2(input).println()
 }
 
 private fun Char.toPriority(): Int =
