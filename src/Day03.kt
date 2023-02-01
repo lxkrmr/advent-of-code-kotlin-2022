f9d40abd99c0c2328cbc66bec0a120ac371c2384
Alexander Kramer <a.kramer@reply.de>
2023-02-01 19:28:32 +0100

    :sparkles: solved Day 03 part 1

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,12 +1,15 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-        input.println()
-        return input.size
+        return input.flatMap { rucksack ->
+            val (firstCompartment, secondCompartment) = rucksack.chunked(rucksack.length / 2)
+            val commonChars = firstCompartment.filter { char -> char in secondCompartment }.toSet()
+            commonChars.map { char -> char.toPriority() }
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-       return input.size
+        return input.size
     }
 
     // test input
@@ -16,10 +19,17 @@ fun main() {
     check(part1(testInput) == 157)
 
     // test part 2: if implementation meets criteria from the description, like:
-    check(part2(testInput) == 12)
+    // check(part2(testInput) == 12)
 
     // use the given input and print the result
     val input = readInputAsLines("Day03")
     part1(input).println()
-    part2(input).println()
+    // part2(input).println()
 }
+
+private fun Char.toPriority(): Int =
+    when {
+        this.isLowerCase() -> this.code - 96
+        this.isUpperCase() -> this.code - 38
+        else -> error("Cant convert $this to priority")
+    }
