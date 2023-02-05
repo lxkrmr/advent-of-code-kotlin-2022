08d2d1c02f8666d33421e5f00f3ec5adccf17a44
Alexander Kramer <a.kramer@reply.de>
2023-02-05 15:45:26 +0100

    :sparkles: solved day 04 part 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -11,25 +11,34 @@ fun main() {
                 }
         }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int =
+        input.count { elvesPair ->
+            elvesPair
+                .split(",")
+                .let { (first, second) ->
+                    val firstRange = first.toIntRange()
+                    val secondRange = second.toIntRange()
+                    firstRange overlapsWith secondRange
+                }
+        }
 
     // test input
     val testInput = readInputAsLines("Day04_test")
     check(part1(testInput) == 2)
-    // check(part2(testInput) == 12)
+    check(part2(testInput) == 4)
 
     // use the given input and print the result
     val input = readInputAsLines("Day04")
     part1(input).println()
-    // part2(input).println()
+    part2(input).println()
 }
+
+private infix fun IntRange.overlapsWith(other: IntRange): Boolean = this.any { int -> int in other }
 
 private fun String.toIntRange(): IntRange {
     val intRangeRegex = Regex("""(\d+)-(\d+)""")
-    val find = intRangeRegex.find(this)
-    val (first, last) = find?.destructured ?: error("Can't convert $this to IntRange")
+    val find = intRangeRegex.find(this) ?: error("Can't convert $this to IntRange")
+    val (first, last) = find.destructured
     return first
         .toInt()
         .rangeTo(last.toInt())
